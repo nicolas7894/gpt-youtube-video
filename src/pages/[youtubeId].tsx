df6f67af24050f4d64b8fe239874a611ece4b8df
Nicolas Surleau <surleaunicolas@gmail.com>
2023-05-04 10:43:33 +0200

    change style

--- src/pages/[youtubeId].tsx
+++ src/pages/[youtubeId].tsx
@@ -1,7 +1,19 @@
 import Chat from '@/components/chat/Chat'
-
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 const Home = () => {
-  return <Chat />
+  return (
+    <>
+      <div className="min-h-full">
+        <main>
+          <div className="mx-auto max-w-7xl  sm:px-6 lg:px-8">
+            <Chat />
+          </div>
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default Home
