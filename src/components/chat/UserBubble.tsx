72194cc66b7e306fe165778c83bcbae9ca177636
Nicolas Surleau <surleaunicolas@gmail.com>
2023-05-04 10:43:33 +0200

    change style

--- src/components/chat/UserBubble.tsx
+++ src/components/chat/UserBubble.tsx
@@ -4,10 +4,7 @@ interface Props {
 const UserBubble = ({ message }: Props) => {
   return (
     <div className="flex flex-row items-center">
-      <div className="border  border-black flex items-center justify-center h-10 w-10 rounded-full bg-white-500 flex-shrink-0 text-black">
-        U
-      </div>
-      <div className="bg-orange-200 relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl bg">
+      <div className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl bg">
         <div style={{ whiteSpace: 'pre-wrap' }}>{message}</div>
       </div>
     </div>
