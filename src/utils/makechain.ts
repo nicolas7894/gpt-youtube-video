3091ddf3217b2c073d440dac015b8ad4b7b9c1f9
Nicolas Surleau <surleaunicolas@gmail.com>
2023-05-10 10:21:57 +0200

    change model

--- src/utils/makechain.ts
+++ src/utils/makechain.ts
@@ -17,7 +17,7 @@ Follow Up Input: {question}
 Standalone question:`)
 
 const QA_PROMPT =
-  PromptTemplate.fromTemplate(`You are a helpful AI assistant. Use the following pieces of context delimited by << >> to answer the question at the end.
+  PromptTemplate.fromTemplate(`You are a helpful AI assistant. Use the following pieces of context delimited by << >> to answer the question.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer. 
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context. The context is from a video transcript.
 
@@ -32,12 +32,9 @@ export const makeChain = (vectorstore: PineconeStore) => {
     prompt: CONDENSE_PROMPT,
   })
 
-  const docChain = loadQAChain(
-    new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' }),
-    {
-      prompt: QA_PROMPT,
-    }
-  )
+  const docChain = loadQAChain(new OpenAI({ temperature: 0 }), {
+    prompt: QA_PROMPT,
+  })
 
   const input: ConversationalRetrievalQAChainInput = {
     retriever: vectorstore.asRetriever(),
